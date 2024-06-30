e9ceddbaeeb35eb891bd47f94bcdcace1a324481
dghcodebase <darshanwork121@gmail.com>
2024-06-30 13:13:10 +0530

    Add files via upload

---
+++ c++/3_digit_num_.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std; 
+
+int main()
+{
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    
+    if( n>99 && n<1000 )
+    {
+        cout<<"Three Digit Number";
+    }
+    else
+    {
+        cout<<"not a three digit number";
+    }
+    
+
+    
+}
